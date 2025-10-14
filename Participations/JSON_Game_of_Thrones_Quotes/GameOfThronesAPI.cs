248e5cc7af22139dab03f219485d37a1ceba71e8
Ackerman, Adam P. <adam@ou.edu>
2025-10-14 09:11:01 -0500

    Completed GOT quotes participation

---
+++ Participations/JSON_Game_of_Thrones_Quotes/GameOfThronesAPI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Game_of_Thrones_Quotes
+{
+    public class GameOfThronesAPI
+    {
+
+        public string sentence { get; set; }
+
+        public Character character { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"{sentence}\"\n{character.name}";
+        }
+    }
+
+    public class Character
+    {
+        public string name { get; set; }
+    }
+}
--- Participations/JSON_Game_of_Thrones_Quotes/MainWindow.xaml.cs
+++ Participations/JSON_Game_of_Thrones_Quotes/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +22,27 @@ namespace JSON_Game_of_Thrones_Quotes
         public MainWindow()
         {
             InitializeComponent();
+            //set the content to tbQuote to "Press the button to get a quote"
+            tbQuote.Text = "Press the button to get a quote";
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new HttpClient();
+            string json = client.GetStringAsync("https://api.gameofthronesquotes.xyz/v1/random").Result;
+
+            GameOfThronesAPI api = JsonConvert.DeserializeObject<GameOfThronesAPI>(json);
+            tbQuote.Text = api.ToString();
+            lstQuotes.Items.Add(api);
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string contentsAsJSON = JsonConvert.SerializeObject(lstQuotes.Items);
+
+            File.WriteAllText("got_quotes.json", contentsAsJSON);
+
+            MessageBox.Show("Quotes saved to got_quotes.json");
         }
     }
 }
