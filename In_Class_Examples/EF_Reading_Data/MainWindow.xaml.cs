8a6b9c2177fab4579a05daf9ac7a8648bd259680
Ackerman, Adam P. <adam@ou.edu>
2025-10-28 09:56:56 -0500

    Did in class participation over EF

---
+++ In_Class_Examples/EF_Reading_Data/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using EF_Reading_Data.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EF_Reading_Data
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DB_128040_practiceContext db = new DB_128040_practiceContext();
+
+            //var games = db.FootballSchedules.ToList();
+
+            foreach (var game in db.FootballSchedules.Where(g => g.IsHomeGame == true && g.Season == 2018))
+            {
+                //if (game.IsHomeGame)
+                //{
+                //    lstHome.Items.Add(game);
+
+                //}
+                lstHome.Items.Add(game);
+            }
+
+            foreach (var game in db.FootballSchedules.Where(x => x.IsHomeGame == false && x.Season == 2018))
+            {
+                lstAway.Items.Add(game);
+            }
+
+        }
+    }
+}
